10c62989ae17680cb93ecc28128922b0e144e88f
Sid1803 <siddheshkmande@gmail.com>
2026-05-16 21:32:24 +0530

    oop task final

--- siddhesh_m/Task3_oop/bank.cpp
+++ siddhesh_m/Task3_oop/bank.cpp
@@ -1,3 +1,5 @@
+#include <cmath>
+#include <exception>
 #include <iostream>
 #include <stdexcept>
 #include <string>
@@ -24,13 +26,33 @@ class InvalidPINException;
 class AccountBlockedException;
 class LoanRejectedException;
 
+class InsufficientBalanceException: public runtime_error {
+    public:
+        InsufficientBalanceException(): runtime_error("Error: Insufficient balance") {}
+};
+
+class InvalidPINException: public runtime_error {
+    public:
+        InvalidPINException(): runtime_error("Error: Invalid pin"){}
+};
+
+class AccountBlockedException: public runtime_error {
+    public:
+        AccountBlockedException(): runtime_error("Error: This account has been blocked"){}
+};
+
+class LoanRejectedException: public runtime_error {
+    public:
+        LoanRejectedException(): runtime_error("Error: Loan was rejected"){}
+};
+
 class Bank {
     public:
         int bankId;
         string bankName;
-        vector<Branch> branches;
-        vector<Customer> customers;
-        vector<Employee> employees;
+        vector<Branch*> branches;
+        vector<Customer*> customers;
+        vector<Employee*> employees;
 };
 
 class Branch {
@@ -39,8 +61,8 @@ class Branch {
         string branchName;
         string IFSCCode;
         string address;
-        vector<Account> accounts;
-        vector<Employee> employees;
+        vector<Account*> accounts;
+        vector<Employee*> employees;
 };
 
 class Customer {
@@ -54,8 +76,9 @@ class Customer {
         string address;
         string aadhaarNumber;
         string PANNumber;
-        vector<Account> accounts;
-        vector<Loan> loans;
+        vector<Account*> accounts;
+        vector<Loan*> loans;
+        double cibilScore;
 };
 
 class Account {
@@ -65,21 +88,62 @@ class Account {
         double balance;
         tm dateOpened;
         string status;
-        Branch branch;
-        Customer customer;
-        vector<Transaction> transactions;
+        Branch* branch;
+        Customer* customer;
+        vector<Transaction*> transactions;
+
+        virtual void deposit(double amt) {
+            balance += amt;
+        };
+
+        virtual void withdraw(double amt) = 0;
+
+        void addTransaction(Transaction* t) {
+            transactions.push_back(t);
+        };
+
+        virtual void showDetails() {
+            cout << "Account Number: " << accountNumber << endl;
+            cout << "Balance: " << balance << endl;
+        };
 };
 
 class SavingsAccount: public Account {
     public:
         double interestRate;
         double minimumBalance;
+
+        void withdraw(double amt) override {
+            if (balance - amt < minimumBalance) {throw InsufficientBalanceException();}
+            else {
+                balance -= amt;
+            }
+        };
+
+        void showDetails() override {
+            Account::showDetails();
+            cout << "Interest rate: " << interestRate << endl;
+            cout << "Minimum balance" << minimumBalance << endl;
+        };
 };
 
 class CurrentAccount: public Account {
     public:
         double overdraftLimit;
         string businessName;
+
+        void withdraw(double amt) override {
+            if (balance - amt < -overdraftLimit) {throw InsufficientBalanceException();}
+            else {
+                balance -= amt;
+            }
+        };
+
+        void showDetails() override {
+            Account::showDetails();
+            cout << "Overdraft limit: " << overdraftLimit << endl;
+            cout << "Business name: " << businessName << endl;
+        };
 };
 
 class FixedDepositAccount: public Account {
@@ -88,6 +152,25 @@ class FixedDepositAccount: public Account {
         tm maturityDate;
         double FDInterestRate;
         int tenureMonths;
+
+        void withdraw(double amt) override {
+            if (amt > FDAmount) {throw InsufficientBalanceException();};
+            if (mktime(&maturityDate) < time(nullptr)) {
+                balance += FDAmount * pow((1 + 1.0*FDInterestRate/100), tenureMonths/12);
+                balance -= amt;
+                status = "Closed";
+            }
+            else {
+                FDAmount -= amt;
+            };
+        };
+
+        void showDetails() override {
+            Account::showDetails();
+            cout << "FD Amount: " << FDAmount << endl;
+            cout << "FD Interest Rate: " << FDInterestRate << endl;
+            cout << "Tenure: " << tenureMonths << " months" << endl;
+        };
 };
 
 class Transaction {
@@ -96,9 +179,47 @@ class Transaction {
         string transactionType;
         double amount;
         tm transactionDate;
-        Account senderAccount;
-        Account receiverAccount;
+        Account* senderAccount = nullptr;
+        Account* receiverAccount = nullptr;
         string status = "Pending";
+
+        bool executeTransaction() {
+            try {
+                if (transactionType == "Withdraw") {
+                    senderAccount->withdraw(amount);
+                }
+                else if (transactionType == "Deposit") {
+                    receiverAccount->deposit(amount);
+                }
+                else if (transactionType == "Transfer") {
+                    senderAccount->withdraw(amount);
+                    receiverAccount->deposit(amount);
+                };
+                status = "Success";
+
+                if (senderAccount != nullptr) {
+                    senderAccount->transactions.push_back(this);
+                }
+                if (receiverAccount != nullptr) {
+                    receiverAccount->transactions.push_back(this);
+                }
+
+                return true;
+
+            } catch (exception& e) {
+                status = "Failed";
+                cout << e.what() << endl;
+
+                if (senderAccount != nullptr) {
+                    senderAccount->transactions.push_back(this);
+                }
+                if (receiverAccount != nullptr) {
+                    receiverAccount->transactions.push_back(this);
+                }
+
+                return false;
+            };
+        };
 };
 
 class Loan {
@@ -110,7 +231,23 @@ class Loan {
         int tenureMonths;
         double EMIAmount;
         string loanStatus = "Pending";
-        Customer customer;
+        Customer* customer;
+
+        void apply() {
+            if (customer->cibilScore < 600 || customer->loans.size() > 0) {
+                loanStatus = "Rejected";
+                throw LoanRejectedException();
+            };
+
+            loanStatus = "Approved";
+
+            double emi;
+            emi = (loanAmount * interestRate/12/100 * pow(1+interestRate/12/100, tenureMonths))/(pow(1+interestRate/12/100, tenureMonths)-1);
+
+            customer->loans.push_back(this);
+            cout << "Loan accepted" << endl;
+            cout << "Amount: " << loanAmount << " " << "EMI: " << emi << endl;
+        }
 };
 
 class ATMCard {
@@ -121,7 +258,29 @@ class ATMCard {
         int PIN;
         string cardType;
         string cardStatus = "Active";
-        Account linkedAccount;
+        Account* linkedAccount = nullptr;
+
+        void checkStatus() {
+            if (linkedAccount->balance < 500) {
+                cardStatus = "Blocked";
+                cout << "ATM card blocked due to low balance" << endl;
+            } 
+        }
+
+        void checkPin(int enteredPin) {
+            checkStatus();
+            if (cardStatus == "Blocked") {
+                throw AccountBlockedException();
+            };
+
+            if (enteredPin == PIN) {
+                cout << "Successfully verified pin" << endl;
+                linkedAccount->showDetails();
+            }
+            else {
+                cout << "Wrong pin" << endl;
+            };
+        };
 };
 
 class Employee {
@@ -166,24 +325,4 @@ class EmailNotification: public Notification {
             cout << subject << endl;
             cout << message << endl;
         }
-};
-
-class InsufficientBalanceException: public runtime_error {
-    public:
-        InsufficientBalanceException(): runtime_error("Error: Insufficient balance") {}
-};
-
-class InvalidPINException: public runtime_error {
-    public:
-        InvalidPINException(): runtime_error("Error: Invalid pin"){}
-};
-
-class AccountBlockedException: public runtime_error {
-    public:
-        AccountBlockedException(): runtime_error("Error: This account has been blocked"){}
-};
-
-class LoanRejectedException: public runtime_error {
-    public:
-        LoanRejectedException(): runtime_error("Error: Loan was rejected"){}
 };
